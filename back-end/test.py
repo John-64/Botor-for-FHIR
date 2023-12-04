54b92c43e981f2f68413e5fd1739d239cb5d7461
GiovanniManfredi <manfredigiovanni64@gmail.com>
2023-12-04 20:18:29 +0100

    20:18

--- back-end/test.py
+++ back-end/test.py
@@ -1,104 +1,28 @@
-#from gpt4all import GPT4All
+import json
+import requests
 
-#PATH='/Users/gianni/Progetti/LLM Models/wizard-vicuna-13b.Q3_K_S.gguf'
+# Esegui la richiesta e converte la risposta in un oggetto JSON
+response = requests.get("https://hapi.fhir.org/baseR4/Observation?subject=Patient/30163&code=3141-9")
+data = response.json()
 
-#model = GPT4All(PATH)
-#output = model.generate(prompt='Give me just the the date of twin tower attack?', temp=0.5, max_tokens=20)
-#print(output)
+# Inizializza un array per contenere le informazioni delle osservazioni
+observations_array = []
 
+# Itera attraverso le osservazioni nel JSON
+for entry in data.get('entry', []):
+    observation_data = entry.get('resource', {})
 
-"""
-import time
-from gpt4all import GPT4All
+    # Estrai i valori desiderati per ogni osservazione
+    observation_id = observation_data.get('id', '')
+    status = observation_data.get('status', '')
+    code_text = observation_data.get('code', {}).get('text', '')
+    effective_date_time = observation_data.get('effectiveDateTime', '')
+    value = observation_data.get('valueQuantity', {}).get('value', '')
+    unit = observation_data.get('valueQuantity', {}).get('unit', '')
 
-PATH='/Users/gianni/Progetti/LLM Models/mistral-7b-v0.1.Q2_K.gguf'
+    # Aggiungi le informazioni dell'osservazione all'array
+    observation_info = [observation_id, status, code_text, effective_date_time, value, unit]
+    observations_array.append(observation_info)
 
-
-model = GPT4All(PATH)
-output = model.generate(prompt='Give me just the the date of twin tower attack?', temp=0.5, max_tokens=200)
-end_time = time.time()
-
-print(output)
-
-generation_time = end_time - start_time
-print(f"\nGeneration time: {generation_time} seconds")
-
-import time
-from gpt4all import GPT4All
-
-PATH='/Users/gianni/Progetti/LLM Models/mistral-7b-v0.1.Q2_K.gguf'
-
-model = GPT4All(PATH)
-start_time = time.time()
-output = model.generate(
-    prompt='Mi dai un metodo di studio efficace?',
-    temp=0.2,
-    max_tokens=200
-)
-end_time = time.time()
-print(output)
-generation_time = end_time - start_time
-print(f"\nGeneration time: {generation_time} seconds")
-
-# Stampa a pezzi
-#tokens = []
-#for token in model.generate("The capital of France is", max_tokens=20, streaming=True):
-#    tokens.append(token)
-#print(tokens)
-
-
-
-# create pieline for QA
-
-from gpt4all import GPT4All
-from llama_cpp import Llama
-from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
-
-PATH='/Users/gianni/Progetti/LLM Models/llama-2-7b-chat.Q2_K.gguf'
-llm = Llama(model_path=PATH)
-#model = GPT4All(PATH)
-
-
-llm_chain = LLMChain(prompt=prompt, llm=llm)
-
-question = "What NFL team won the Super Bowl in the year Justin Bieber was born?"
-
-llm_chain.run(question)
-
-from langchain.llms import CTransformers
-
-PATH='/Users/gianni/Progetti/LLM Models/mistral-7b-v0.1.Q2_K.gguf'
-
-llm = CTransformers(model=PATH, model_type='mistral')
-
-print(llm('AI is going to'))
-"""
-import time
-from gpt4all import GPT4All
-
-PATH='/Users/gianni/Progetti/LLM Models/mistral-7b-instruct-v0.1.Q4_K_M.gguf'
-
-model = GPT4All(PATH)
-
-start_time = time.time()
-
-#question = "Rispondi Dammi un metodo di studio efficace per lo studio all'università"
-
-#output = model.generate(prompt="Answer this question always in the same language of the question: {Question}", temp=0.35)
-
-question = "Come stai?"
-
-prompt = f"Answer just at this question always in Italian: {question}"
-
-output = model.generate(prompt=prompt, temp=0.35)
-
-end_time = time.time()
-
-print(output)
-
-generation_time = end_time - start_time
-
-print(f"\nAnswer took: %.2f seconds"%generation_time)
-
-# Prova a implementarlo in Flask
+# Stampare l'array con le informazioni di tutte le osservazioni
+print(observations_array)
--- flask-environment/botor.py
+++ flask-environment/botor.py
@@ -1,11 +1,68 @@
 import flask
-from gpt4all import GPT4All
-import re
-
-def remove_answer_prefix(text):
-  return re.sub("Answer: ", "", text)
+from openai import OpenAI
+from langchain.llms import LlamaCpp
+import requests
+import json
 
 app = flask.Flask(__name__)
+
+# Defining a function for extract the main information from a question made in natural language and return a FHIR query
+def generate_query(question):
+    # Insert here you API key
+    API_KEY = "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"
+
+    # Check out all the models here: https://platform.openai.com/docs/models/overview
+    GPT_MODEL = "gpt-3.5-turbo-1106"
+
+    # Setting up the client
+    client = OpenAI(api_key = API_KEY)
+
+    messages = [
+        {"role": "system", "content": 'Follow this form for build the query: "https://hapi.fhir.org/baseR4/replace-with-information"'
+        },
+        {"role": "user", "content": question},
+        {"role": "system", "content": 'Now study the request and give me just the query, without other symbols or explaination (that because i will save it in a variabile). Replace the information you want to add in "replace-with-information". '
+        },
+    ]
+
+    response = client.chat.completions.create(
+        model=GPT_MODEL,
+        messages=messages,
+        max_tokens=50,
+        temperature=0
+    )
+
+    query = response.choices[0].message.content
+
+    return query
+
+# This function will help me to understand if i can print the graph or not
+def check_json_fields(json_data):
+    try:
+        # Carica il JSON
+        data = json.loads(json_data)
+
+        # Verifica se l'array "entry" è presente
+        if 'entry' in data:
+            # Itera attraverso gli oggetti in "entry"
+            for entry in data['entry']:
+                # Verifica se i campi necessari sono presenti in ciascun oggetto "resource"
+                if 'resource' in entry and 'effectiveDateTime' in entry['resource'] and 'valueQuantity' in entry['resource']:
+                    return True
+            # Nessun oggetto in "entry" con i campi necessari trovato
+            return False
+        else:
+            # Se l'array "entry" non è presente, ritorna False
+            return False
+
+    except json.JSONDecodeError as e:
+        print(f"Errore durante il parsing del JSON: {e}")
+        return False
+
+def generate_chart(json):
+    da = json
+    return da
+    #-----
 
 @app.route('/')
 def home():
@@ -13,20 +70,46 @@ def home():
 
 @app.route('/process', methods=['POST']) 
 def process():
-    PATH='/Users/gianni/Progetti/LLM Models/gpt4all-falcon-q4_0.gguf'
+    # Replace with the path of your model
+    PATH='/Users/gianni/Progetti/LLM Models/mistral-7b-instruct-v0.1.Q4_K_M.gguf'
+    # Setting up the LLM for my MacBook Air M1 - 8GB
+    n_gpu_layers = 1
+    n_batch = 512
+    n_ctx = 2048
 
+    llm = LlamaCpp(
+        model_path=PATH,
+        n_gpu_layers=n_gpu_layers,
+        n_ctx=n_ctx,
+        n_batch=n_batch,
+        verbose=False,
+        f16_kv=True
+    )
+
+     # Taking the question from the user
     data = flask.request.get_json()
     question = data['value']
 
-    model = GPT4All(PATH)
+    query = generate_query(question)
+    print("This is the query created by OpenAI: " + query) ## Just testing
 
-    prompt = f"My name is Botor, your chatbot, and i will try to answer to all your question. This is the question: {question}"
+    # Server resource fetching
+    json_answer = requests.get(query).text
 
-    output = model.generate(prompt=question, temp=0.75)
+    if check_json_fields(json_answer):
+        print("Il JSON contiene effectiveDateTime e valueQuantity.") ## Just testing
+        return flask.jsonify(result="Qui ci sarà il grafo")
+    else:
+        print("Il JSON non contiene entrambi i campi necessari.") ## Just testing
+        prompt = json_answer + "\n Take this patient information and provide me with a well-composed an human-readable sentence."
 
-    response = remove_answer_prefix(output)
-    return flask.jsonify(result=response)
+        result = llm(prompt)
+        print(result)
+        return flask.jsonify(result=result)
+
+    # return flask.jsonify(result=result)
 
 if __name__ == '__main__':
     app.debug = True
+    # You can change the port as you prefer
     app.run(host='0.0.0.0', port=9000, threaded=True)
